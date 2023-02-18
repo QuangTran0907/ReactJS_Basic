131caed04403657bb9009e652ca868b74d6d8191
Quang <tranhuuquang2k1@gmail.com>
2023-02-18 23:14:05 +0700

    create component

---
+++ src/views/example/mycomponent.js
@@ -0,0 +1,10 @@
+import React from 'react';
+
+class MyComponent extends React.Component {
+    render() {
+        return (
+            <div>Day la react component</div>
+        )
+    }
+}
+export default MyComponent;
