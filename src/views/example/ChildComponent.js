c05f55ce0b256fdb4f28a3964ace4d030699fc08
Quang <tranhuuquang2k1@gmail.com>
2023-02-22 10:40:36 +0700

    class and funciton

--- src/views/example/ChildComponent.js
+++ src/views/example/ChildComponent.js
@@ -1,8 +1,7 @@
 import React from 'react';
-import ChildComponent from './ChildComponent';
 
 
-class MyComponent extends React.Component {
+class ChildComponent extends React.Component {
     // state = {
     //     name: "Quang",
     //     channel: "Quang toi"
@@ -36,12 +35,7 @@ class MyComponent extends React.Component {
 
     state = {
         fistName: '',
-        lastName: '',
-        arrayJobs: [
-            { id: 'abc', tittle: 'developer', salaly: '500' },
-            { id: 'def', tittle: 'tester', salaly: '400' },
-            { id: 'ghi', tittle: 'Gu', salaly: '800' }
-        ]
+        lastName: ''
     }
     handleChangeFistName = (event) => {
         this.setState({
@@ -62,28 +56,49 @@ class MyComponent extends React.Component {
 
     render() {
 
+        console.log("Check props: ", this.props)
+        // let name = this.props.name;
+        // let age = this.props.age;
+        let { name, age, arrayJobs } = this.props;/*Ten bien trung ten key */
+
         return (
+
             <div>
-                <p>hello html form</p>
-                <form action="">
-                    <label htmlFor="fname">First name:</label><br />
-                    <input
-                        type="text"
-                        value={this.state.fistName}
-                        onChange={(event) => this.handleChangeFistName(event)}
-                    />
-                    <br />
-                    <label htmlFor="lname">Last name:</label><br />
-                    <input type="text" value={this.state.lastName} onChange={(event) => this.handleChangeLastName(event)} /><br /><br />
-                    <input type="button" value="Submit" onClick={(event) => this.handleSubmit()} />
-                    <ChildComponent name={this.state.fistName} age={"22"} arrayJobs={this.state.arrayJobs} />
 
-                </form>
+                <div className='Jobs-list'>
+                    {
+                        arrayJobs.map((item, index) => {
+                            return (
+                                <div key={item.id}>
+                                    {item.tittle} -- {item.salaly}
+                                </div>
+                            )
+                        })
+                    }
 
+                </div>
 
             </div>
 
         )
     }
 }
-export default MyComponent;
+// const ChildComponent = (props) => {
+//     let arrayJobs = props.a;
+//     return (
+
+//         <div className='Jobs-list'>
+//             {
+//                 arrayJobs.map((item, index) => {
+//                     return (
+//                         <div key={item.id}>
+//                             {item.tittle} -- {item.salaly}
+//                         </div>
+//                     )
+//                 })
+//             }
+
+//         </div>
+//     )
+// }
+export default ChildComponent;
